825de8f18380c847664e02e791c76b7b97e4385a
Dana Clarke III <87629562+DanaClarkeIII@users.noreply.github.com>
2023-06-28 09:44:47 -0400

    Update Bosch_BMI088_new_data_interrupt_setup.ino
    Added comments

--- code components/Bosch_BMI088_new_data_interrupt_setup/Bosch_BMI088_new_data_interrupt_setup.ino
+++ code components/Bosch_BMI088_new_data_interrupt_setup/Bosch_BMI088_new_data_interrupt_setup.ino
@@ -1,22 +1,28 @@
 #include <Wire.h>
 
-#define ACCEL_ADDRESS 0x18
+// Define accelerometer and gyroscope I2C addresses
+#define ACCEL_ADDRESS 0x18 
 #define GYRO_ADDRESS 0x69
 
+// Define accelerometer and gyroscope interrupt mapping register addresses
 #define ACCEL_INT_MAP_REG 0x58
 #define GYRO_INT_MAP_REG 0x18
 
 void setup() {
-  Wire.begin();
-  Wire.setClock(400000); // Set I²C clock speed to 400kHz
+  Wire.begin(); // Initiate the Wire library and join the I2C bus as a master
 
+  Wire.setClock(400000); // Set I²C clock speed to 400kHz.
+
+  // Write to the accelerometer and gyroscope interrupt mapping registers.
+  // This sets up how interrupts from the devices will be mapped and handled.
   writeRegister(ACCEL_ADDRESS, ACCEL_INT_MAP_REG, 0x04);
   writeRegister(GYRO_ADDRESS, GYRO_INT_MAP_REG, 0x01);
 }
 
+// Function to write a single byte of data to a specific register in the device.
 void writeRegister(uint8_t deviceAddress, uint8_t registerAddress, uint8_t data) {
   Wire.beginTransmission(deviceAddress);
-  Wire.write(registerAddress);
-  Wire.write(data);
+  Wire.write(registerAddress); // Send the register address to the device
+  Wire.write(data); // Write the data to the register
   Wire.endTransmission();
 }
